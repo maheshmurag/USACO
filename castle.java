8e820d5871201c393daa183af4e65b0360f5d938
Mahesh Murag <maheshmmurag@gmail.com>
2015-07-20 20:57:06 -0700

    commented explanations for castle.java

--- castle.java
+++ castle.java
@@ -3,6 +3,13 @@ TASK: castle
 LANG: JAVA
 ID: maheshm2
  */
+/*
+Loops through each space, runs dfs on it if it hasn't been assigned. Loops through spaces for the second time to
+determine the best walls to remove in order to make the biggest combined room.
+
+dfs is recursively called on the 4 spaces around a space and if there's no wall between current space and next space,
+it assigns the same room id to the next space.
+ */
 
 import java.io.*;
 import java.util.ArrayList;
@@ -25,17 +32,17 @@ public class castle {
         N = (int) input.nval;
         arr = new int[N][M];
         comps = new int[N][M];
-        cmps = new int[2501];
-        for (int i = 0; i < arr.length; i++) {
+        cmps = new int[2501];//stores size of rooms (index == room id, value = how many spaces are in that room)
+        for (int i = 0; i < arr.length; i++) {//reads input
             for (int j = 0; j < arr[0].length; j++) {
                 input.nextToken();
                 arr[i][j] = (int) input.nval;
             }
         }
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {//loop through castle
             for (int j = 0; j < arr[0].length; j++) {
-                if (comps[i][j] == 0) {
-                    c++;
+                if (comps[i][j] == 0) {//if a space hasnt been assigned a room, run dfs on it
+                    c++;//increments after a room has been filled with spaces
                     comps[i][j] = c;
                     dfs(i, j, c);
                 }
@@ -44,12 +51,12 @@ public class castle {
         }
         for (int i = 0; i < arr.length; i++)
             for (int j = 0; j < arr[0].length; j++)
-                dfs(i, j, comps[i][j]);
-        Arrays.sort(cmps);
+                dfs(i, j, comps[i][j]);//called for 2nd time to determine optimal walls to remove
+        Arrays.sort(cmps);//sorts rooms by size
         Thing[] optimalsArr = new Thing[optimals.size()];
         optimalsArr = optimals.toArray(optimalsArr);
-        Arrays.sort(optimalsArr);
-        Thing best = optimalsArr[optimalsArr.length - 1];
+        Arrays.sort(optimalsArr);//sorts the optimal walls with the compareTo function below
+        Thing best = optimalsArr[optimalsArr.length - 1];//gets best wall
         output.println(c);
         output.println(cmps[cmps.length - 1]);
         output.println(best.score);
@@ -57,16 +64,16 @@ public class castle {
         output.close();
     }
 
-    public static void dfs(int i, int j, int comp) {
+    public static void dfs(int i, int j, int comp) {//recursive function
         int tmpx, tmpy;
-        for (int x = 0; x < dirs.length; x++) {
+        for (int x = 0; x < dirs.length; x++) {//loops through four directions in dirs
             tmpx = i + dirs[x][0];
             tmpy = j + dirs[x][1];
-            if (withinBounds(tmpx, tmpy)) {
-                if (!a(arr[i][j], x) && comps[tmpx][tmpy] == 0) {
+            if (withinBounds(tmpx, tmpy)) {//within arr bounds and >= 0
+                if (!a(arr[i][j], x) && comps[tmpx][tmpy] == 0) {//if there's no wall & it hasnt been processed
                     comps[tmpx][tmpy] = comp;
                     dfs(tmpx, tmpy, comp);
-                } else if (a(arr[i][j], x))
+                } else if (a(arr[i][j], x))//if there's a wall, then check if its optimal to remove it
                     if (comps[tmpx][tmpy] != comps[i][j] && (cmps[comps[tmpx][tmpy]] + cmps[comp] >= maxcomp)) {
                         optimals.add(new Thing(i + 1, j + 1, cmps[comps[tmpx][tmpy]] + cmps[comp], x));
                         maxcomp = cmps[comps[tmpx][tmpy]] + cmps[comp];
@@ -75,7 +82,7 @@ public class castle {
         }
     }
 
-    static class Thing implements Comparable<Thing> {
+    static class Thing implements Comparable<Thing> {//represents optimal wall to remove to get biggest room
         int i, j, score, dir;
 
         public Thing(int x, int y, int s, int d) {
@@ -85,7 +92,7 @@ public class castle {
             dir = d;
         }
 
-        public int compareTo(Thing o2) {
+        public int compareTo(Thing o2) {//sorts optimal walls by room size, south, west, and direction ('N'>'E')
             if (this.score == o2.score) {
                 if (this.j == o2.j) {
                     if (this.i == o2.i)
@@ -96,11 +103,11 @@ public class castle {
         }
     }
 
-    public static boolean withinBounds(int x, int y) {
+    public static boolean withinBounds(int x, int y) {//checks if x and y are within array bounds
         return ((x < N && x >= 0) && (y < M && y >= 0));
     }
 
-    public static boolean a(int n, int d) {
+    public static boolean a(int n, int d) {//checks if a wall exists in a certain direction (can be improved with '&')
         switch (d) {
             case 0:
                 return (n == 2 || n == 3 || n == 6 || n == 7 || n == 10 || n == 11 || n == 14 || n == 15);
