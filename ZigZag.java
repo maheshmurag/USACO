fe7d51ed8a6c83ee894e72584aeda995db2a2a29
Mahesh Murag <maheshmmurag@gmail.com>
2015-07-24 14:01:15 -0700

    zigzag

--- ZigZag.java
+++ ZigZag.java
@@ -1,23 +1,42 @@
 import java.util.Arrays;
 
-public class Maximum_Decreasing_Subsequence {
+public class ZigZag {
     public static void main(String[] args) {
-        //from http://train.usaco.org/usacotext2?a=IlchiKxkPYK&S=dp
-        int arr[] = {10, 22, 9, 33, 21, 50, 41, 60};
-//        int arr[] = {5, 3, 4, 8, 6, 7};
-        int state[] = new int[arr.length];
-        Arrays.fill(state, 1);//value at i represents longest increasing sequence up till that index
+        //from http://community.topcoder.com/stat?c=problem_statement&pm=1259&rd=4493
+        int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        Thing state[] = new Thing[arr.length];
+        for (int i = 0; i < state.length; i++) {
+            state[i] = new Thing();
+        }
         int n = arr.length;
-        int max = 0;
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < i; j++)
-                if (arr[i] >= arr[j])
-                    if (state[j] + 1 >= state[i]) {
-                        state[i] = state[j] + 1;
-                        if(state[i]>max)
-                            max=state[i];
+        int max = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if ((arr[i] - arr[j] > 0 && arr[j] - arr[state[j].indLast] < 0) || (arr[i] - arr[j] < 0 && arr[j] - arr[state[j].indLast] > 0)) {
+                    if (state[j].len + 1 >= state[i].len) {
+                        state[i].indLast = j;
+                        state[i].len = state[j].len + 1;
+                        if (state[i].len > max) {
+                            max = state[i].len;
+                        }
                     }
-        System.out.println(max);
-        System.out.println(Arrays.toString(state));
+                }
+            }
+        }
+        System.out.println(max + 1);
+    }
+
+    static class Thing {
+        int len = 1;
+        int indLast = 0;
+
+        public Thing() {
+            len = 1;
+            indLast = 0;
+        }
+
+        public String toString() {
+            return "len: " + len + " indLast: " + indLast;
+        }
     }
 }
