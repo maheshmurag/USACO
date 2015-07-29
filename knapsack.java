c88852e1c0050e8cb30886dc4971561820606f89
Mahesh Murag <maheshmmurag@gmail.com>
2015-07-28 17:21:04 -0700

    made knapsack more robust

--- knapsack.java
+++ knapsack.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 
 public class knapsack{
     public static void main(String[] args) {
-        int[] weight = {1, 3, 4, 5};
-        int[] val = {1, 4, 5, 7};
-        int maxWeight = 7;
+        int[] val = {10, 40, 30, 50};//{1, 3, 4, 5};
+        int[] weight = {5, 4, 6, 3};//{1, 4, 5, 7};
+        int maxWeight = 10;//7;
         int[][] g = new int[weight.length][maxWeight + 1];
         for (int i = 0; i < g.length; i++)
             Arrays.fill(g[i], 0);
@@ -14,9 +14,9 @@ public class knapsack{
             for (int j = 1; j < g[0].length; j++) {
                 if (i == 0 && weight[i] <= j)
                     g[i][j] = val[i];
-                else if (weight[i] > j)
+                else if (i-1>=0 && weight[i] > j)
                     g[i][j] = g[i - 1][j];
-                else
+                else if(i-1>=0)
                     g[i][j] = Math.max(val[i] + g[i - 1][j - weight[i]], g[i - 1][j]);
             }
         }
@@ -24,7 +24,7 @@ public class knapsack{
         //retrace
         int i = g.length - 1, j = g[0].length - 1;
         String str = "";
-        while (g[i][j] != 0) {
+        while (i>=0 && g[i][j] != 0) {
             if (i - 1 >= 0 && g[i][j] != g[i - 1][j]) {
                 str += "item " + i + " with value " + val[i] + " and weight " + weight[i] + "\n";
                 j -= weight[i];
