12bbbd049bd51fbc2d1313f7f3fb6a1782c18a40
Mahesh Murag <maheshmmurag@gmail.com>
2015-08-07 12:17:44 -0700

    fix template, added prefix.java

---
+++ USACO/prefix.java
@@ -0,0 +1,25 @@
+/*
+TASK: template
+LANG: JAVA
+ID: maheshm2
+ */
+import java.io.*;
+
+
+public class template {
+    public static void main(String[] args) throws IOException {
+        String prob = "template";
+        StreamTokenizer input =new StreamTokenizer(new BufferedReader(new FileReader(prob+".in")));
+        PrintWriter output=new PrintWriter(new FileWriter(prob+".out"));
+        input.nextToken();
+        int n=(int)input.nval;
+        output.println();
+        output.close();
+
+    }
+    static int next() throws IOException {
+        input.nextToken();
+        return (int) input.nval;
+    }
+}
+
