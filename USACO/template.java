a534910e4dfe2d47791a6f09c73a715585ddfff2
Mahesh Murag <maheshmmurag@gmail.com>
2015-08-09 17:21:57 -0700

    working nocows

--- USACO/template.java
+++ USACO/template.java
@@ -5,15 +5,12 @@ ID: maheshm2
  */
 import java.io.*;
 
-
 public class template {
     static StreamTokenizer input;
     public static void main(String[] args) throws java.io.IOException {
         String prob = "template";
         input =new StreamTokenizer(new BufferedReader(new FileReader(prob+".in")));
         PrintWriter output=new PrintWriter(new FileWriter(prob+".out"));
-
-
 
         output.println();
         output.close();
