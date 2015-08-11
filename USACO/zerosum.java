0b2eba1a23ab2ef876ae36754fc599a6faafb234
Mahesh Murag <maheshmmurag@gmail.com>
2015-08-10 20:01:57 -0700

    finishe dworking zerosum

--- USACO/zerosum.java
+++ USACO/zerosum.java
@@ -18,38 +18,35 @@ public class zerosum {
         String prob = "zerosum";
         input = new StreamTokenizer(new BufferedReader(new FileReader(prob + ".in")));
         PrintWriter output = new PrintWriter(new FileWriter(prob + ".out"));
-
         n = nextInt();
         s = new ArrayList<String>();
-        rec(1, 1, "1");
+        rec(1, 1, "1", false);
         String[] arr = new String[s.size()];
         s.toArray(arr);
         Arrays.sort(arr);
         for (int i = 0; i < arr.length; i++)
-            System.out.println(arr[i]);
+            output.println(arr[i]);
         output.close();
     }
-
-    static void rec(int index, int val, String str) {
-        if (index == n) {
-            if (val == 0) {
-                if (!s.contains(str)) {
+    static void rec(int index, int val, String str, boolean a) {
+        if (index >= n) {
+            if (val == 0)
+                if (!s.contains(str))
                     s.add(str);
-                }
-            }
-//            else {
-//                System.out.println(val + ":" + str);
-//            }
             return;
         }
-        rec(index + 1, val + (index + 1), str + "+" + (index + 1));
-        rec(index + 1, val - (index + 1), str + "-" + (index + 1));
-//        if(str.lastIndexOf("+")>str.lastIndexOf("-"))
-//            rec(index + 1, val + index + 1, str + " " + index);
-//        else
-//            rec(index + 1, val - index + 1, str + " " + index);
+        rec(index + 1, val + (index + 1), str + "+" + (index + 1),false);
+        rec(index + 1, val - (index + 1), str + "-" + (index + 1),false);
+        if (a) return;
+        if(index==1){
+            rec(index + 1, index * 10 + (index + 1), str + " " + (index + 1), true);
+            return;
+        }
+        if (str.lastIndexOf("+") > str.lastIndexOf("-"))
+            rec(index + 1, val - index + index * 10 + (index + 1), str + " " + (index + 1), true);
+        else
+            rec(index + 1, val + index - (index * 10 + (index + 1)), str + " " + (index + 1),true);
     }
-
     static int nextInt() throws IOException {
         input.nextToken();
         return (int) input.nval;
