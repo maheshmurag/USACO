128fbb09815f1f34eb3c39dd09828e188a08b729
Mahesh Murag <maheshmmurag@gmail.com>
2015-08-11 15:56:07 -0700

    shorter money

--- USACO/money.java
+++ USACO/money.java
@@ -7,30 +7,22 @@ import java.io.*;
 import java.util.Arrays;
 public class money {
     static int V,N;
-    static long[][] dp;
-    static int[] arr;
+    static long[] nways;
     static StreamTokenizer input;
     public static void main(String[] args) throws IOException {
         String prob = "money";
         input =new StreamTokenizer(new BufferedReader(new FileReader(prob+".in")));
         PrintWriter output=new PrintWriter(new FileWriter(prob+".out"));
         V = nextInt();N = nextInt();
-        arr = new int[V];
-        for (int i = 0; i < V; i++)
-            arr[i] = nextInt();
-        dp = new long[V+1][N+1];
-        for (int i = 0; i < dp.length; i++)
-            for (int j = 0; j < dp[0].length; j++) {
-                dp[i][j] =-1;
-        output.println(rec(V-1, N));
+        nways = new long[N+1];
+        nways[0] = 1;
+        for (int i = 0; i < V; i++) {
+            int x = nextInt();
+            for (int j = x; j <= N; j++)
+                nways[j] += nways[j-x];
+        }
+        output.println(nways[N]);
         output.close();
-    }
-    static long rec(int m, int n){
-        if(m<0 || n<0) return 0;
-        if(n==0) return 1;
-        if(dp[m][n]!=-1) return dp[m][n];
-        dp[m][n] = rec(m - 1, n)+rec(m, n - arr[m]);
-        return dp[m][n];
     }
     static int nextInt() throws IOException {
         input.nextToken();
