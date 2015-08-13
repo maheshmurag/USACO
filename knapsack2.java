be5ab89efd3ce0cd7fb2987cc7a536c9bafe71ef
Mahesh Murag <maheshmmurag@gmail.com>
2015-08-13 14:51:05 -0700

    commented knapsack2

--- knapsack2.java
+++ knapsack2.java
@@ -12,23 +12,22 @@ public class knapsack2 {
         int val[] = {6, 10, 12};
         int wt[] = {1, 2, 3};
         int W = 5;
-        int[][] arr = new int[wt.length][W+1];
+        int[][] arr = new int[wt.length][W + 1];
         for (int i = 0; i < arr.length; i++)
             Arrays.fill(arr[i], 0);
-        for (int i = 1; i < arr.length; i++)
-            for (int j = 0; j < arr[0].length; j++)
-                if(wt[i] <= j)
-                    arr[i][j] = val[i];
+        for (int j = 0; j < arr[0].length; j++)//initializes first row
+            if (wt[0] <= j)
+                arr[0][j] = val[0];
         for (int i = 1; i < arr.length; i++) {
             for (int j = 0; j < arr[0].length; j++) {
-                if(wt[i] > j)
-                    arr[i][j] = arr[i-1][j];
+                if (wt[i] > j)
+                    arr[i][j] = arr[i - 1][j];//if item i can't fit, use best value when excluding this item (i.e. i-1)
                 else
-                    arr[i][j] = Math.max(arr[i-1][j], val[i] + arr[i-1][j-wt[i]]);
+                    arr[i][j] = Math.max(arr[i - 1][j], val[i] + arr[i - 1][j - wt[i]]);
                 //max of (without this item but same capacity, or with this item & capacity reduced by item's weight)
             }
         }
-        System.out.println(arr[arr.length-1][arr[0].length-1]);
+        System.out.println(arr[arr.length - 1][arr[0].length - 1]);
     }
 }
 
