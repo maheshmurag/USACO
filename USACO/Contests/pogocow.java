abdb128e02788c9f679ec794f9264f14fea428e3
Mahesh Murag <maheshmmurag@gmail.com>
2015-12-13 10:45:26 -0800

    minor changes

--- USACO/Contests/pogocow.java
+++ USACO/Contests/pogocow.java
@@ -4,8 +4,6 @@ import java.util.Arrays;
 
 public class pogocow {
     static StreamTokenizer input;
-    static int[] x;
-    static int[] p;
     static Thing[] cows;
     static int[][] dp;
     static int n;
@@ -15,80 +13,77 @@ public class pogocow {
         input = new StreamTokenizer(new BufferedReader(new FileReader(prob + ".in")));
         PrintWriter output = new PrintWriter(new FileWriter(prob + ".out"));
         n = nextInt();
-        x = new int[n];
-        p = new int[n];
         cows = new Thing[n];
-        for (int i = 0; i < n; i++) {
-//            x[i] = nextInt();
-//            p[i] = nextInt();
+        for (int i = 0; i < n; i++)
             cows[i] = new Thing(nextInt(), nextInt());
-        }
         Arrays.sort(cows);
-        dp = new int[2][n + 1];
-
+        dp = new int[n + 1][n + 1];
         for (int i = 0; i < dp.length; i++)
             for (int j = 0; j < dp[0].length; j++)
-                dp[i][j] = -1;
-
-//        for (int i = 1; i < n - 1; i++) {
-//            for (int j = 1; j <= i; j++) {
-//                if(i-j >= 0)
-//                    dp(i - j, i, false, p[i]);
-//                if(i + j < n)
-//                    dp(i + j, i, true, p[i]);
-//            }
-//        }
-
-        dp(0, 4, true, p[4]);
-
-        int max = Integer.MIN_VALUE;
-        for (int i = 1; i < n - 1; i++) {
-            int tmp = Math.max(dp[1][i], dp[0][i]);
-            if (tmp > max) max = tmp;
-        }
-//        System.out.println("left: " + Arrays.toString(dp[0]));
-//        System.out.println("right: " + Arrays.toString(dp[1]));
-        output.close();
-    }
-
-    static class Thing implements Comparable<Thing>{
-        int x, p;
-        public Thing(int xx, int pp){
-            x = xx;
-            p = pp;
-        }
-        public int compareTo(Thing o){
-            return this.x-o.x;
-        }
-        public String toString(){
-            return x+":"+p;
-        }
-    }
-
-    public static int dp(int ci, int prev, boolean dir, int points) {//true or 1 = right, false or 0= left
-        if (ci < 0 || ci >= n ){
-//            dp[dir?1:0][ci] = points;
-            return points;
-        }
-        System.out.println(x[prev]+" to "+x[ci]+":"+(dir?"right":"left")+":"+points +":"+dp[dir?1:0][ci]);
-        if (dir) {
-            if (dp[1][ci] != -1) return dp[1][ci];
-            for (int i = 0; i < n; i++) {
-                if (x[i] > x[ci]) {
-                    if (Math.abs(x[i] - x[ci]) >= Math.abs(x[ci] - x[prev])) {
-                        dp[1][ci] = dp(i, ci, true, points + p[ci]);
-                    }
+                dp[i][j] = 0;
+        for (int ii = 1; ii >= -1; ii -= 2) {
+            for (int i = (ii == 1 ? n - 1 : 0); (ii == 1 ? i >= 0 : i < n); i -= ii) {
+                for (int j = (ii == 1 ? 0 : i - 1); (ii == 1 ? j < i : j >= 0); j += ii) {
+                    int m = Integer.MIN_VALUE;
+                    for (int k = j + ii; (ii == 1 ? k < i : k >= 0); k += ii)
+                        if (dp[j][k] < m && Math.abs(cows[j].x - cows[i].x) <= Math.abs(cows[k].x - cows[j].x))
+                            m = dp[j][k];
+                    dp[i][j] = Math.max(cows[i].p + m, dp[i][j]);
                 }
             }
         }
-        else {
-            if (dp[0][ci] != -1) return dp[0][ci];
-            for (int i = 0; i < n; i++)
-                if (x[i] < x[ci] && Math.abs(x[i] - x[ci]) >= Math.abs(x[ci] - x[prev]))//if next-cur >= cur-prev
-                    dp[0][ci] = dp(i, ci, false, points + p[ci]);
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp.length; j++) {
+                if (dp[i][j] > max)
+                    max = dp[i][j];
+            }
         }
-        return points+p[ci];
+        System.out.println(max);
+        output.close();
     }
+
+    static class Thing implements Comparable<Thing> {
+        int x, p;
+
+        public Thing(int xx, int pp) {
+            x = xx;
+            p = pp;
+        }
+
+        public int compareTo(Thing o) {
+            return this.x - o.x;
+        }
+
+        public String toString() {
+            return x + ":" + p;
+        }
+    }
+
+//    public static int dp(int ci, int prev, boolean dir, int points) {//true or 1 = right, false or 0= left
+//        if (ci < 0 || ci >= n ){
+////            dp[dir?1:0][ci] = points;
+//            return points;
+//        }
+//        System.out.println(x[prev]+" to "+x[ci]+":"+(dir?"right":"left")+":"+points +":"+dp[dir?1:0][ci]);
+//        if (dir) {
+//            if (dp[1][ci] != -1) return dp[1][ci];
+//            for (int i = 0; i < n; i++) {
+//                if (cows[i].x > x[ci]) {
+//                    if (Math.abs(cows[i].x - x[ci]) >= Math.abs(x[ci] - x[prev])) {
+//                        dp[1][ci] = dp(i, ci, true, points + p[ci]);
+//                    }
+//                }
+//            }
+//        }
+//        else {
+//            if (dp[0][ci] != -1) return dp[0][ci];
+//            for (int i = 0; i < n; i++)
+//                if (cows[i].x < x[ci] && Math.abs(cows[i].x - x[ci]) >= Math.abs(x[ci] - x[prev]))//if next-cur >= cur-prev
+//                    dp[0][ci] = dp(i, ci, false, points + p[ci]);
+//        }
+//        return points+p[ci];
+//    }
 
     static int nextInt() throws IOException {
         input.nextToken();
